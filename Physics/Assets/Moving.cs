7bca12ded3f3ab47e6d3701c9a5abe2652909eb9
Wendy Zhang <wzhanghu@ucsc.edu>
2026-04-24 19:07:05 -0700

    Changed script of moving obstacle

--- Physics/Assets/Moving.cs
+++ Physics/Assets/Moving.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class MovingObstacle : MonoBehaviour
 {
-    [Header("Movement Settings")]
     public float moveDistance = 5f;
     public float speed = 2f;
 
     private Vector3 startPos;
+    private Rigidbody rb;
 
     void Start()
     {
-        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+        startPos = rb.position;
+
+        rb.isKinematic = true;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector3 right = transform.right;
 
         float movement = Mathf.Sin(Time.time * speed) * moveDistance;
 
-        transform.position = startPos + right * movement;
+        Vector3 targetPos = startPos + right * movement;
+
+        rb.MovePosition(targetPos);
     }
 }
